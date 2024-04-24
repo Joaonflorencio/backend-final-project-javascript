b41059d2b6a2209e707425292216383dff6afd0a
João Neto <jflorencioadv@gmail.com>
2024-04-25 01:39:57 +0200

    codigo

--- config/config.js
+++ config/config.js
@@ -1,13 +1,17 @@
+require('dotenv').config(); 
 const mongoose = require('mongoose');
 
+const MONGO_URI = process.env.MONGO_URI;
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected...');
-    } catch (error) {
-        console.error('Connection to MongoDB failed:', error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Encerrar o processo com falha
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
