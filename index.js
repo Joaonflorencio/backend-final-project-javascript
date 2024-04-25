16e1af080da127163de947d9a916f7d4ef522242
João Neto <jflorencioadv@gmail.com>
2024-04-25 02:19:35 +0200

    teste

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dbConnection = require('./config/config')
+const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5480
+const routes = require('./routes/routes');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', routes);
+
+dbConnection()
+
+app.listen(PORT, () => {
+  console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
+})
